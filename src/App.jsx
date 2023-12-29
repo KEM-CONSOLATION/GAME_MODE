ee8cf121444b128e901040caa24711210d593001
Kem-consolation <consolationlotachi@gmail.com>
2023-12-29 08:30:25 +0100

    resolving conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
 import "./App.css";
-import { useState } from "react";
-import Single from "./assets/Single.png";
-import Challenge from "./assets/Challenge.png";
-import MultiPlayer from "./assets/MultiPlay.png";
-import Dot from "./assets/Dot.png";
-import Soccer from "./assets/Soccer.png";
-import Man from "./assets/Man.png";
-import Woman from "./assets/Woman.png";
 import SIdeBar from "./Components/SIdeBar";
 import Dashboard from "./Components/Dashboard";
 
